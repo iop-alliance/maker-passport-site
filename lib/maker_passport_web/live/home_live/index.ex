59512aab0ca9787078c8e80f3d4bf9cd00e69f40
Max Wardeh <max@wardeh.org>
2024-11-11 16:22:34 +0000

    Update layout and home page

---
+++ lib/maker_passport_web/live/home_live/index.ex
@@ -0,0 +1,20 @@
+defmodule MakerPassportWeb.HomeLive.Index do
+  use MakerPassportWeb, :live_view
+
+  on_mount {MakerPassportWeb.UserAuth, :mount_current_user}
+
+  import Ecto.Query, warn: false
+
+  alias MakerPassport.Maker
+
+  @impl true
+  def mount(_params, _session, socket) do
+    latest_profiles = Maker.list_profiles(limit: 4, sort: %{sort_by: :updated_at, sort_order: :desc})
+
+    socket =
+      socket
+      |> assign(latest_profiles: latest_profiles)
+
+    {:ok, socket}
+  end
+end
