aeffbeb190998b00ae91b13b9cc1431dcb624625
hifzapu <hifzapu@gmail.com>
2024-12-21 14:22:01 +0500

    Format code

--- lib/maker_passport_web/live/visitor_live/index.ex
+++ lib/maker_passport_web/live/visitor_live/index.ex
@@ -31,41 +31,40 @@ defmodule MakerPassportWeb.VisitorLive do
   end
 
   def handle_event("confirm_account", %{"visitor" => %{"token" => token}}, socket) do
-      case Visitor.get_visitor_by_token(token) do
-        nil ->
-          {:noreply,
-            socket
-            |> put_flash(:error , "Confirmation link is invalid or it has expired.")
-            |> redirect(to: ~p"/")
+    case Visitor.get_visitor_by_token(token) do
+      nil ->
+        {:noreply,
+         socket
+         |> put_flash(:error, "Confirmation link is invalid or it has expired.")
+         |> redirect(to: ~p"/")}
+
+      %{is_verified: true} ->
+        {:noreply, socket |> redirect(to: ~p"/")}
+
+      visitor ->
+        {:ok, _updated_visitor} = Visitor.update_visitor(visitor, %{is_verified: true})
+
+        # Process emails
+        emails = Visitor.list_emails_of_a_visitor(visitor.id)
+        Visitor.delete_emails_of_a_visitor(visitor.id)
+
+        # Send emails to makers
+        emails
+        |> Enum.each(fn email ->
+          email_params = %{
+            sender_email: visitor.email,
+            maker_email: email.profile.user.email,
+            subject: email.subject,
+            body: email.body
           }
 
-        %{is_verified: true} ->
-          {:noreply, socket |> redirect(to: ~p"/")}
+          UserNotifier.send_email_to_maker(email_params)
+        end)
 
-        visitor ->
-          {:ok, _updated_visitor} = Visitor.update_visitor(visitor, %{is_verified: true})
-
-          # Process emails
-          emails = Visitor.list_emails_of_a_visitor(visitor.id)
-          Visitor.delete_emails_of_a_visitor(visitor.id)
-
-          # Send emails to makers
-          emails |> Enum.each(fn email ->
-            email_params = %{
-              sender_email: visitor.email,
-              maker_email: email.profile.user.email,
-              subject: email.subject,
-              body: email.body
-            }
-            UserNotifier.send_email_to_maker(email_params)
-
-          end)
-
-          {:noreply,
-            socket
-            |> put_flash(:info, "All you pending emails have been sent to makers.")
-            |> redirect(to: ~p"/")
-          }
-      end
+        {:noreply,
+         socket
+         |> put_flash(:info, "All you pending emails have been sent to makers.")
+         |> redirect(to: ~p"/")}
+    end
   end
 end
